b2788aeaae05c35688e8d982bb59dafe0531bf18
smiath <smiath.yt@gmail.com>
2025-06-10 11:58:19 +0200

    merge

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,13 +1,15 @@
 import { Link, Outlet, useLocation } from "react-router";
+import Footer from "../footer/Footer";
 
 export default function Layout() {
     const location = useLocation().pathname;
 
     return (
-        <>
-            <div>
+        <div className="layout-container">
+            <div className="content">
                 <Outlet />
             </div>
-        </>
+            <Footer />
+        </div>
     );
 }

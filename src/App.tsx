57b2687f56e9d94587371ed9fbf348b2ebb0e741
smiath <smiath.yt@gmail.com>
2025-06-10 12:47:03 +0200

    fix merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import './App.css'
-import { Route, Routes } from 'react-router'
-import Reservation from './components/reservation/reservation'
 import LoadCardList from "./components/LoadCardList.tsx";
 
 function App() {
 
   return (
     <>
-      <Routes>
-        <Route path="/reservation" element={<Reservation />} />
-      </Routes>
-        <LoadCardList/>
+      <LoadCardList />
     </>
   )
 }
